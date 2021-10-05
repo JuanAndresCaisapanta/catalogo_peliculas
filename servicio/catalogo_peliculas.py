fbbe76d90b77bd150febd5da0e695edf5c64018b
JuanAndresCaisapanta <juancaisapanta@hotmail.com>
2021-10-05 01:31:25 -0500

    Clase de testeo y fin de proyecto

--- servicio/catalogo_peliculas.py
+++ servicio/catalogo_peliculas.py
@@ -6,12 +6,13 @@ class CatalogoPeliculas:
 
     @classmethod
     def agregar_pelicula(cls,pelicula):
-        with open(cls.ruta_archivo,'a',encodings='utf8') as archivo:
+        with open(cls.ruta_archivo,'a',encoding='utf8') as archivo:
             archivo.write(f'{pelicula.nombre}\n')
     @classmethod
     def listar_peliculas(cls):
-        with open(cls.ruta_archivo,'a',encodings='utf8') as archivo:
+        with open(cls.ruta_archivo,'r',encoding='utf8') as archivo:
             print('Catalogo de Peliculas'.center(50,'-'))
+            print(archivo.read())
     @classmethod
     def eliminar_peliculas(cls):
         os.remove(cls.ruta_archivo)
---
+++ test_catalogo_peliculas.py
@@ -0,0 +1,28 @@
+from dominio.Pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp
+
+opcion =None
+
+while opcion != 4:
+    try:
+        print('opciones: ')
+        print('1. Agregar Peliculas')
+        print('2. Listar Peliculas')
+        print('3. Eliminar catalogo peliculas')
+        print('4. Salir')
+        opcion = int(input('escribe tu opcion (1-4): '))
+
+        if opcion ==1:
+            nombre_pelicula = input('proporciona nombre de la pelicula: ')
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_pelicula(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas()
+        elif opcion == 3:
+            cp.eliminar_peliculas()
+    except Exception as e:
+        print(f'Ocurrio un error {e}')
+        opcion=None
+
+else:
+    print('Salimos del programa')
